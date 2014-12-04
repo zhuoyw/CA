3d1b0e1bba495894853137c2a1a5c66f34170d31
jichenhui2012 <you@yourdomain.example.com>
2014-12-04 18:25:53 +0800

    pipeline_tb changed

--- pipeline_tb.vhd
+++ pipeline_tb.vhd
@@ -5,7 +5,7 @@ ENTITY pipeline_tb IS
 END pipeline_tb;
  
 ARCHITECTURE behavior OF pipeline_tb IS 
-    COMPONENT pipeline
+    COMPONENT partpipe
     port (
 	clk				: in std_logic;
 	rst				: in std_logic;
@@ -19,11 +19,12 @@ ARCHITECTURE behavior OF pipeline_tb IS
 	
 	signal clock : std_logic := '0';
 	signal reset : std_logic := '0';
+	signal num : integer range 0 to 15 := 0; 
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: pipeline PORT MAP (clock,reset,inst);
+   uut: partpipe PORT MAP (clock,reset,inst);
 
    -- Clock process definitions
    process
@@ -31,21 +32,21 @@ BEGIN
 		wait for clocktime/2;
 		clock <= '1';
 		wait for clocktime/2;
-		clock <= '0';
-		wait for clocktime/2;
-		clock <= '1';
-		wait for clocktime/2;
-		clock <= '0';
-		wait for clocktime/2;
-		clock <= '1';
-		wait for clocktime/2;
-		clock <= '0';
-		wait for clocktime/2;
-		clock <= '1';
-		wait for clocktime/2;
-		clock <= '0';
+		clock <= '0'; 
+		if num = 15 then
+			num <= 0;
+		else
+			num <= num + 1;
+		end if;
    end process;
- 
 
+   process(num)
+   begin
+    	if num = 15 then 
+        	inst <= "0100100100000010";
+    	elsif num = 3 or num = 7 or num = 11 then
+        	inst <= "0100000101000110";
+    	end if;
+ 	end process;
    -- Stimulus process
 END;
